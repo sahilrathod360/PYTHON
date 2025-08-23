e4ca437b082148d1c04ec4e94a4568b94ae22e06
Anup Vishwakarma <155341007+YoAnupOP@users.noreply.github.com>
2025-08-23 10:03:43 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,6 @@
+#4.	Write a python program to add two numbers using command line arguments. 
+
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+sum = first_number + second_number
+print("The sum of {0} and {1} is {2}".format(first_number, second_number, sum))
