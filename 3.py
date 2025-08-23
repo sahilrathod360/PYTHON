65fc9031a17272de919c7353931830d9f272efaa
Anup Vishwakarma <155341007+YoAnupOP@users.noreply.github.com>
2025-08-23 10:03:23 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,5 @@
+#3.	Write a program that evaluates an expression given by the user at run time using eval() function. 
+# Example Enter and expression: 6+5-3*2-(6*2).
+
+x=input("Enter any expression:")
+print (eval(x))
